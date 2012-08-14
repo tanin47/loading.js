e6d77c91f439e359ffdbbe124e6ff399f4806935
tanin <tanin@tanin-desktop.(none)>
2012-08-14 14:39:38 +0700

    version 1.0

---
+++ README.md
@@ -0,0 +1,40 @@
+loading.js
+====================
+
+This javascript solves:
+
+    When a user clicks on a submit button in order to submit a form,
+    How would you show a loading image? Next to the button? inside the button?
+
+I've decided that the loading image should be shown within the button, and the label is erased, because the user won't be able to re-click it until that loading image disappears.
+
+If there is an error, I show the error message, remove the loading image, and bring back the label of the button.
+
+Please see [the demo](http://tanin.nanakorn.com/b/182/loading_button_js)
+
+
+Requirement
+-----------------
+
+* jQuery
+* A button must be a SPAN (or DIV) element
+
+
+How to use it
+------------------
+
+You can invoke ```$(button).loading_button(true);``` in order to show the loading image, and you can invoke ```$(button).loading_button(false);``` in order to bring it back to the normal state.
+
+It also works with a textbox and an icon. Please see [the demo](http://tanin.nanakorn.com/b/182/loading_button_js) for more information.
+
+
+Author
+-------------
+
+Tanin Na Nakorn
+
+
+Boring Legal stuff
+--------------------
+
+It's under Tanin license, which means you can do whatever you want with it, and I cannot sue you.
---
+++ jquery.loading_button.js
@@ -0,0 +1,118 @@
+(function($){
+
+ 	$.fn.extend({
+
+		loading_icon:function(enabled)
+		{
+			return this.each(function() {
+
+				if (enabled == true)
+				{
+					if (this.loading == true) return;
+					this.loading = true;
+					this.save_background = $(this).css('background-image');
+					this.save_background_repeat = $(this).css('background-repeat');
+					this.save_background_position = $(this).css('background-position');
+
+					$(this).css('background-image','url(/loading_button/loading.gif)');
+					$(this).css('background-repeat', 'no-repeat');
+
+					var x = ($(this).width() - 16) / 2;
+					var y = ($(this).height() - 11) / 2;
+
+					$(this).css('background-position', x + 'px ' + y + 'px');
+
+				}
+				else
+				{
+					if (this.loading == undefined || this.loading == false) return;
+					this.loading = false;
+					$(this).html('');
+					$(this).css('background-image',this.save_background);
+					$(this).css('background-position', this.save_background_position);
+					$(this).css('background-repeat', this.save_background_repeat);
+				}
+
+    		});
+
+		},
+
+		//pass the options variable to the function
+ 		loading_button: function(enabled,options) {
+
+
+			//Set the default values, use comma to separate the settings, example:
+			var defaults = {
+				image : '<img src="/loading_button/loading.gif">',
+				word: ''
+			}
+
+			var options =  $.extend(defaults, options);
+
+    		return this.each(function() {
+
+				if (enabled == true)
+				{
+					if (this.loading === true) return;
+					this.loading = true;
+
+					this.placeholder = $('<div>');
+					var previous_children = $(this).contents();
+					for (var i=0;i<previous_children.length;i++) {
+						$(this.placeholder).append(previous_children[i]);
+					}
+
+					$(this).html('<span style="display:inline-block;width:100%;height:100%;" onclick="if (event.stopPropagation) event.stopPropagation();event.cancelBubble = true;return false;">'+options.image + ' ' + options.word+'</span>');
+				}
+				else
+				{
+					if (this.loading !== true) return;
+					this.loading = false;
+
+					if (this.placeholder == undefined) return;
+
+					$(this).html('');
+					var previous_children = $(this.placeholder).contents();
+					for (var i=0;i<previous_children.length;i++) {
+						$(this).append(previous_children[i]);
+					}
+
+					delete this.placeholder;
+				}
+
+    		});
+		},
+
+
+		//pass the options variable to the function
+ 		loading_textbox: function(enabled, options) {
+
+
+			return this.each(function() {
+
+				if (enabled == true)
+				{
+					if (this.loading == true) return;
+					this.loading = true;
+					this.save_background = $(this).css('background-image');
+					$(this).css('background-image','url(/loading_button/loading.gif)');
+					$(this).css('background-position','98% 5px');
+					$(this).css('background-repeat','no-repeat');
+
+				}
+				else
+				{
+					if (this.loading == undefined || this.loading == false) return;
+					this.loading = false;
+
+					$(this).css('background-image', this.save_background);
+					$(this).css('background-position','');
+					$(this).css('background-repeat','');
+				}
+
+    		});
+    	}
+	});
+
+})(jQuery);
+
